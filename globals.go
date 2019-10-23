ed3fdb13c3acf3f3a5e52d44ca5ac37b6fe82c1a
Tomas Berger <tomashb@ntnu.no>
2019-10-23 20:38:22 +1100

    Add a go "enum" of eventtypes for webhooks

--- globals.go
+++ globals.go
@@ -1,9 +1,25 @@
 package imt2681ass2
 
-import "time"
+import (
+	"time"
+	"strings"
+)
 
 //The time used to find the uptime of this program
 var startTime time.Time
 
 //Version of this current API
 const Version string = "v1"
+
+type EventMsg string
+
+const (
+	//The event is message
+	Commit EventMsg = "commit"
+	//the event is languges
+	Languages EventMsg = "languages"
+	//The event is issues
+	Issues EventMsg = "issues"
+	//The event is status
+	Status EventMsg = "status"
+)
