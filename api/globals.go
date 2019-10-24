6eedd2581a1a9e26433c9507b69a115fcae886ee
Tomas Berger <tomashb@ntnu.no>
2019-10-25 03:19:16 +1100

    Renamed Conflicting names, and root path for API

--- api/globals.go
+++ api/globals.go
@@ -9,15 +9,18 @@ var startTime time.Time
 //Version of this current API
 const Version string = "v1"
 
+//Root API Call
+const GITAPI string = "https://git.gvk.idi.ntnu.no/api/v4/"
+
 type EventMsg string
 
 const (
 	//The event is message
-	Commit EventMsg = "commit"
+	CommitE EventMsg = "commit"
 	//the event is languges
-	Languages EventMsg = "languages"
+	LanguagesE EventMsg = "languages"
 	//The event is issues
-	Issues EventMsg = "issues"
+	IssuesE EventMsg = "issues"
 	//The event is status
-	Status EventMsg = "status"
+	StatusE EventMsg = "status"
 )
