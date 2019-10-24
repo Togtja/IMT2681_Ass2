04e7de98a445200bd67f7dc1b0bbc373ab48c53d
Tomas Berger <tomashb@ntnu.no>
2019-10-25 03:18:27 +1100

    Removed reduntant struct classes

--- api/structs.go
+++ api/structs.go
@@ -2,26 +2,47 @@ package api
 
 type Repos struct{
 	Repos []Repo `json:"repos"`
-	auth bool `json:"auth"`
+	Auth bool `json:"auth"`
 }
 type Repo struct{
-	repository string `json:"repository"`
-	commits int `json:"commits"`
+	Repository string `json:"repository"`
+	Commits int `json:"commits"`
 }
 type Lang struct{
-	language []string `json:"languages"`
-	auth bool `json:"auth"`
+	Language []string `json:"languages"`
+	Auth bool `json:"auth"`
 }
 type Users struct{
 	Users []User `json:"users"`
 }
 type User struct{
-	username string `json:"username"`
-	count int `json:"count"`
+	Username string `json:"username"`
+	Count int `json:"count"`
 }
 type StatusDiag struct{
 	Gitlab int `json:"gitlab"`
 	Database int `json:"database"`
 	Uptime string `json:"uptime"`
 	Version string `json:"version"`
+}
+//The lengs of it's array is the number of commits
+
+//Details regarding the commit
+type Commit struct{
+	Msg string `json:"message"`
+	Author string `json:"author_name"`
+}
+//A single project
+type Project struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Owner []Owner `json:"owner"`
+
+}
+//Details about an Owner
+type Owner struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Username string `json:"username"`
+	State string `json:"state"`
 }
