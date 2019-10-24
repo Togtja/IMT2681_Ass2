36f96ce09ff65a5f3d899444d44e35af832d48ac
Tomas Berger <tomashb@stud.ntnu.no>
2019-10-24 18:59:17 +0200

    Change strcut to me more golang friendly

--- api/structs.go
+++ api/structs.go
@@ -1,48 +1,56 @@
 package api
 
-type Repos struct{
+//Repos Represent the Commit repos
+type Repos struct {
 	Repos []Repo `json:"repos"`
-	Auth bool `json:"auth"`
+	Auth  bool   `json:"auth"`
 }
-type Repo struct{
+
+//Repo represent a single repositiry
+type Repo struct {
 	Repository string `json:"repository"`
-	Commits int `json:"commits"`
+	Commits    int    `json:"commits"`
 }
-type Lang struct{
+
+//Lang Represent the language structure
+type Lang struct {
 	Language []string `json:"languages"`
-	Auth bool `json:"auth"`
+	Auth     bool     `json:"auth"`
 }
-type Users struct{
-	Users []User `json:"users"`
-}
-type User struct{
+
+//User Represent the user
+type User struct {
 	Username string `json:"username"`
-	Count int `json:"count"`
+	Count    int    `json:"count"`
 }
-type StatusDiag struct{
-	Gitlab int `json:"gitlab"`
-	Database int `json:"database"`
-	Uptime string `json:"uptime"`
-	Version string `json:"version"`
+
+//Status is the diganostic and status tools
+type Status struct {
+	Gitlab   int    `json:"gitlab"`
+	Database int    `json:"database"`
+	Uptime   string `json:"uptime"`
+	Version  string `json:"version"`
 }
+
 //The lengs of it's array is the number of commits
 
-//Details regarding the commit
-type Commit struct{
-	Msg string `json:"message"`
+//Commit Details regarding the commit
+type Commit struct {
+	Msg    string `json:"message"`
 	Author string `json:"author_name"`
 }
-//A single project
-type Project struct{
-	Id int `json:"id"`
-	Name string `json:"name"`
-	Owner []Owner `json:"owner"`
 
+//Project details concerning the project
+type Project struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Owner []Owner `json:"owner"`
 }
-//Details about an Owner
-type Owner struct{
-	Id int `json:"id"`
-	Name string `json:"name"`
+
+//Owner details about the owner of a Project
+type Owner struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
 	Username string `json:"username"`
-	State string `json:"state"`
+	State    string `json:"state"`
 }
