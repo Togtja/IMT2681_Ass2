7bfbae3dd6c675ea442df8e027719fe8840c3aa8
Tomas Berger <tomashb@ntnu.no>
2019-10-25 03:19:49 +1100

    Add CommitHandler

--- api/apihandlers.go
+++ api/apihandlers.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strings"
 	"encoding/json"
+	"io/ioutil"
+	"strconv"
 )
 
 //NilHandler throws a Bad Request
@@ -13,7 +15,66 @@ func NilHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Default Handler: Invalid request received.")
 	http.Error(w, "Invalid request", http.StatusBadRequest)
 }
+func CommitsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.Header.Add(w.Header(), "content-type", "application/json")
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 5 {
+			fmt.Println("Length is: ", len(parts))
+			http.Error(w, "Expecting format .../", http.StatusBadRequest)
+			return
+		}
+		limittxt := r.FormValue("limit")
+		limit, err := strconv.ParseInt(limittxt, 10, 32)
+		if err != nil && limittxt != ""{
+			errmsg :=  "Invalid limit, error: " + err.Error()
+			http.Error(w, errmsg, http.StatusBadRequest)
+			return;
+		}
+		if limit <= 0 {
+			//Default limit
+			limit = 5
+		}
+		offsettxt := r.FormValue("offset")
+		offset, err := strconv.ParseInt(offsettxt, 10, 32)
+		if err != nil && offsettxt != ""{
+			errmsg :=  "Invalid offset, error: " + err.Error()
+			http.Error(w, errmsg, http.StatusBadRequest)
+			return
+		}
+		auth := r.FormValue("auth") //TODO: Validate this
+		//TEMP
+		fmt.Println(offset, auth)
 
+
+		//TODO Cache all projects
+		resp,  err := http.Get(GITAPI + "projects")
+		if err != nil {
+			errmsg :=  "The HTTP request failed with error" + err.Error()
+			http.Error(w, errmsg, http.StatusInternalServerError)
+			return
+		}
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			errmsg :=  "The HTTP request failed with error" + err.Error()
+			http.Error(w, errmsg, http.StatusInternalServerError)
+			return
+		}
+		var projects []Project
+		err = json.Unmarshal(data, &projects)
+		if err != nil {
+			errmsg :=  "The HTTP request failed with error" + err.Error()
+			http.Error(w, errmsg, http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(projects)
+
+		return
+	}
+	//There is only Get method to commits
+	http.Error(w, "only get method allowed", http.StatusNotImplemented)
+	return
+}
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		http.Header.Add(w.Header(), "content-type", "application/json")
@@ -25,15 +86,15 @@ func StatusHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		gitlab,	 err := http.Get("https://git.gvk.idi.ntnu.no/api/v4/projects")
 		if err != nil {
-			http.Error(w, "The HTTP request failed with error", http.StatusInternalServerError)
-			fmt.Printf("The HTTP request failed with error %s\n", err)
+			errmsg :=  "The HTTP request failed with error" + err.Error()
+			http.Error(w, errmsg, http.StatusInternalServerError)
 			return
 		}
 
 		db, err := http.Get("https://restcountries.eu/rest/v2/")
 		if err != nil {
-			http.Error(w, "The HTTP request failed with error", http.StatusInternalServerError)
-			fmt.Printf("The HTTP request failed with error %s\n", err)
+			errmsg :=  "The HTTP request failed with error" + err.Error()
+			http.Error(w, errmsg, http.StatusInternalServerError)
 			return
 		}
 
--- cmd/gitlabAPI.go
+++ cmd/gitlabAPI.go
@@ -16,7 +16,7 @@ func main() {
 		port = "8080"
 	}
 	http.HandleFunc("/", api.NilHandler)
-	//http.HandleFunc("/repocheck/v1/commits/", imt2681ass2.CountryHandler)
+	http.HandleFunc("/repocheck/v1/commits/", api.CommitsHandler)
 	//http.HandleFunc("/repocheck/v1/languages/", imt2681ass2.SpeciesHandler)
 	//http.HandleFunc("/repocheck/v1/issues/", imt2681ass2.CountryHandler)
 	http.HandleFunc("/repocheck/v1/status/", api.StatusHandler)
